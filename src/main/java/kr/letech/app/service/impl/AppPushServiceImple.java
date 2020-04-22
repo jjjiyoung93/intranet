c54e70799c337e097afcebfe2d509381b32a5d39
jwchoi <jwchoi@letech.kr>
2020-04-22 08:58:07 +0000

    gcm 서버 오류로 사용 정지 추후 계제 예정

--- src/main/java/kr/letech/app/service/impl/AppPushServiceImple.java
+++ src/main/java/kr/letech/app/service/impl/AppPushServiceImple.java
@@ -42,23 +42,27 @@ public class AppPushServiceImple implements AppPushService {
 	}
 
 	public void googlePushSendPayment(Map<String, Object> params) throws Exception {
+		/* gcm 에러 확인으로 일단 gcm 사용 정지
+		 * 20200422 jwchoi
+		 *  
+		 *  */
 
-		// 구글 푸시 발송될 폰 토큰 정보 조회
-		List<String> list = appPushDAO.getSelectAppPushListPayment(params);
-
-		if (list.size() > 0) {
-			String title = (String) params.get("title");
-			String bbsSeq = (String) params.get("bbs_seq");
-			String aprv_no = (String) params.get("aprv_no");
-			String line_chk = (String) params.get("line_chk");
-			String conf_yn = (String) params.get("conf_yn");
-
-			Message message = new Message.Builder().addData("title", "[LETECH] 확인할 정보가 있습니다.")
-					.addData("aprvNo", aprv_no).addData("lineChk", line_chk).addData("confYn", conf_yn).build();
-
-			GCMServerSide gcm = new GCMServerSide();
-
-			gcm.sendMessage(message, list);
-		}
+		/*
+		 * // 구글 푸시 발송될 폰 토큰 정보 조회 List<String> list =
+		 * appPushDAO.getSelectAppPushListPayment(params);
+		 * 
+		 * if (list.size() > 0) { String title = (String) params.get("title"); String
+		 * bbsSeq = (String) params.get("bbs_seq"); String aprv_no = (String)
+		 * params.get("aprv_no"); String line_chk = (String) params.get("line_chk");
+		 * String conf_yn = (String) params.get("conf_yn");
+		 * 
+		 * Message message = new Message.Builder().addData("title",
+		 * "[LETECH] 확인할 정보가 있습니다.") .addData("aprvNo", aprv_no).addData("lineChk",
+		 * line_chk).addData("confYn", conf_yn).build();
+		 * 
+		 * GCMServerSide gcm = new GCMServerSide();
+		 * 
+		 * gcm.sendMessage(message, list); }
+		 */
 	}
 }
